c47b06dda2bdf41c8a4495bca81fb465f9670b36
konika <konikajain1572001@gmail.com>
2024-04-19 12:26:21 +0530

    fourth commit

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+// // const form = document.getElementsByTagName('form')[0];
+// // console.log(form);
+// // window.alert(form);
+// document.getElementById('form_button').addEventListener('click',()=>{
+//     const city = document.getElementById('city').value;
+//     // console.log(city)
+//     if (city === 'hyderabad') {
+//         document.getElementById('hyderabad').style.display = "block";
+//         document.getElementById('branch').style.display = "none";
+//         console.log('selected')
+//     }
+//     // else if (city === 'banglore') {
+//     //     document.getElementById('banglore').style.display = "block";
+//     //     document.getElementById('branch').style.display = "none";
+//     // }
+// })
